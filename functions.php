4d71cd762eaef2e75460fbadc22021483bb14e85
cakecoong <cake.nakarin@gmail.com>
2021-07-13 20:14:46 +0700

    Define Element

--- functions.php
+++ functions.php
@@ -142,8 +142,8 @@ add_action( 'widgets_init', 'sevenmarch_widgets_init' );
 function sevenmarch_scripts() {
 	wp_enqueue_style( 'sevenmarch-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'sevenmarch-style', 'rtl', 'replace' );
-
 	wp_enqueue_script( 'sevenmarch-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'sevenmarch-fontawesome', 'https://kit.fontawesome.com/3d4f27b3b0.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -27,7 +27,7 @@ if ( ! function_exists( 'sevenmarch_posted_on' ) ) :
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'sevenmarch' ),
+			'<i class="fas fa-calendar-alt"></i> %s',
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
@@ -43,7 +43,7 @@ if ( ! function_exists( 'sevenmarch_posted_by' ) ) :
 	function sevenmarch_posted_by() {
 		$byline = sprintf(
 			/* translators: %s: post author. */
-			esc_html_x( 'by %s', 'post author', 'sevenmarch' ),
+			'<i class="fas fa-user"></i> %s',
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
@@ -63,19 +63,19 @@ if ( ! function_exists( 'sevenmarch_entry_footer' ) ) :
 			$categories_list = get_the_category_list( esc_html__( ', ', 'sevenmarch' ) );
 			if ( $categories_list ) {
 				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'sevenmarch' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				printf( '<i class="fas fa-folder"></i> <span class="cat-links">%1$s</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			}
 
 			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'sevenmarch' ) );
+			$tags_list = get_the_tag_list( '', esc_html_x( ' ', 'list item separator', 'sevenmarch' ) );
 			if ( $tags_list ) {
 				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'sevenmarch' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				printf( '<i class="fas fa-tags"></i> </span> <span class="tags-links badge">%1$s</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			}
 		}
 
 		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-			echo '<span class="comments-link">';
+			echo '<span class="comments-link"><i class="fas fa-comment"></i> ';
 			comments_popup_link(
 				sprintf(
 					wp_kses(
